b4896835e61d21c40ba24d8d1d95824c4692886d
hy-git-111 <hyeyoung.k111@gmail.com>
2025-03-14 16:17:50 +0900

    파일 경로 변경으로 인한 삭제
    1. csv_reader.py
    2. csv_writer.py

--- 이론 정리/csv_reader.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open("data.csv", "r", encoding="utf-8") as f:  # data.csv를 r모드로 열기
-    reader = csv.reader(f)  # csv 파일을 작성하는 reader 객체 생성 
-    for row in reader:
-        print(row)
