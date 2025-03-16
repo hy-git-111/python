f44a8cf0d0f909e1f6b724e04fea7ba55d5e0950
hy-git-111 <hyeyoung.k111@gmail.com>
2025-03-17 00:21:37 +0900

    Revert "pre-project 파일 업로드"
    This reverts commit 91e69cfb37a69a12b4523b073dc8b9304909a9ba.

--- 엘리스 SW QA 트랙/pre-project/coupang/tests/pages/temp.py
+++
@@ -1,7 +0,0 @@
-EMAIL_ID = "login-email-input"
-PASSWORD_ID = "login-password-input"
-LOGIN_BTN_CLASS_NAME = "_loginSubmitButton"
-email_to_str_list = list(EMAIL_ID)
-password_to_str_list = []
-
-print(email_to_str_list)
