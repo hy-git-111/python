f434d787e7e6e51b6add78955483e448af772f57
hy-git-111 <hyeyoung.k111@gmail.com>
2025-04-29 17:59:33 +0900

    실행 함수명, 파일명 반환 함수 추가

---
+++ practice_POM/src/utils/utils.py
@@ -0,0 +1,11 @@
+import sys, os
+
+@staticmethod
+def get_current_function(n):
+    return sys._getframe(n).f_code.co_name
+
+@staticmethod
+def get_current_file(n):
+    file_path = sys._getframe(n).f_code.co_filename # 전체 경로 반환
+    file_name = os.path.basename(file_path) # 파일명 반환
+    return os.path.splitext(file_name)[0] # splittext() return ('test_main_page', 'py')
