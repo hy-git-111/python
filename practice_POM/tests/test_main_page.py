d20e9a975550171fcb645368f662081a883859a9
hy-git-111 <hyeyoung.k111@gmail.com>
2025-04-29 20:49:57 +0900

    random_urils() 호출 방법 변경, url 인코딩

--- practice_POM/tests/test_main_page.py
+++ practice_POM/tests/test_main_page.py
@@ -1,21 +1,25 @@
 from src.utils.error_handler import Handler
 from src.pages.main_page import MainPage
-from selenium.webdriver.remote.webdriver import WebDriver
+from src.utils.random_utils import RandomUtils
+from urllib.parse import quote
 
 class TestMainPage():
     def test_header_search(self, driver):
         self.main_page = MainPage(driver)
         self.handler = Handler(driver)
+        self.random_utils = RandomUtils()
 
         self.handler.start_log()
 
         try:
-            header_el = self.main_page.header_search()
+            keyword = self.random_utils.generate_random_product()
+            header_el = self.main_page.header_search(keyword)
 
             assert header_el.text == "검색결과"
-            assert "search.do?" in self.driver.current_url
-        except:
-            self.handler.error_handler()
+            assert quote(keyword) in driver.current_url
+
+        except Exception as e:
+            self.handler.error_handler(e)
 
         finally:
             self.handler.finish_log()
