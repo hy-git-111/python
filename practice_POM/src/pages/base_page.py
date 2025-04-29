5d5dd138563601bf340c68cf1de45840eaaedc09
hy-git-111 <hyeyoung.k111@gmail.com>
2025-04-29 20:50:16 +0900

    오타 수정, 주석 추가

--- practice_POM/src/pages/base_page.py
+++ practice_POM/src/pages/base_page.py
@@ -7,21 +7,25 @@ class BasePage():
     def __init__(self, driver):
         self.driver = driver
 
+    # Explicit wite을 포함한 요소 탐색 함수
     def find_el(self, locator):
         return WebDriverWait(self.driver, 10).until(
-            EC.invisibility_of_element_located(locator)
+            EC.visibility_of_element_located(locator)
         )
-
+    
+    # Explicit wite을 포함한 클릭 함수
     def click(self, locator):
         WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(locator)
         ).click()
 
+    # Explicit wite을 포함한 텍스트 입력 함수
     def input_text(self, locator, text):
         WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located(locator)
         ).send_keys(text)
 
+    # Explicit wite을 포함한 엔터 함수
     def send_enter_key(self, locator):
         WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(locator)
