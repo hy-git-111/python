f7ecdc32713e9cd6140db80788cd79312ca3c4f9
hy-git-111 <hyeyoung.k111@gmail.com>
2025-04-29 14:12:14 +0900

    log의 함수명 호출 오류 수정

--- practice_POM/src/utils/error_handler.py
+++ practice_POM/src/utils/error_handler.py
@@ -2,28 +2,30 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 from selenium.webdriver.remote.webdriver import WebDriver
 import inspect
+import sys
 
-# @pytest.fixture(scope="function", autouse=True)
 class Handler():
     def __init__(self, driver:WebDriver):
         self.driver = driver
+        self.logger = logging.getLogger()
 
+    @staticmethod
     def get_current_function():
-        return inspect.currentframe.f_code.co_name
+        return sys._getframe(3).f_code.co_name
 
-    def log_handler(self):
+    def log_handler(self, message):
         func_name = self.get_current_function()
+        print(func_name)
 
         # 기본 로거 설정
-        logger = logging.getLogger()
-        logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter(f"%(asctime)s - %(levelname)s - {func_name} - %(message)s")
+        self.logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(f"%(asctime)s - %(levelname)s - %{func_name}s - %{message}s")
         
         # 스트림 핸들러 설정
         stream_handler = logging.StreamHandler()
         stream_handler.setLevel(logging.INFO)
         stream_handler.setFormatter(formatter)
-        logger.addHandler(stream_handler)
+        self.logger.addHandler(stream_handler)
 
         # 시간 기반 로테이팅 핸들러 설정
         timed_handler = TimedRotatingFileHandler(
@@ -35,15 +37,16 @@ class Handler():
         timed_handler.suffix = "-%Y%m%d"    # 백업 시 접미사에 날짜 추가
 
     def start_log(self):
-        self.log_handler.logger.info("테스트 시작")
         print("테스트 시작")
+        self.log_handler(message="테스트 시작")
 
     def finish_log(self):
-        self.log_handler.logger.into("테스트 종료")
         print("테스트 종료")
+        self.log_handler(message="테스트 시작")
 
     def error_handler(self, e):
-        self.log_handler.logger.error(f"오류 발생: {e}")
+        print(f"오류 발생: {e}")
+        self.log_handler(message=f"오류 발생: {e}")
         self.driver.save_screenshot("./reports/d.png")
 
     
