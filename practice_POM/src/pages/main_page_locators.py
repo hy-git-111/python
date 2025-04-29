21794fa6d8ede09c415ad64091f4f6de91722a84
hy-git-111 <hyeyoung.k111@gmail.com>
2025-04-29 20:42:42 +0900

    경로 이동으로 인한 추가

---
+++ practice_POM/src/pages/main_page_locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+
+SEARCH_BTN = (By.CLASS_NAME, 'srch')
+SEARCH_INPUT = (By.ID, 'search-box')
---
+++ practice_POM/src/pages/search_page_locator.py
@@ -0,0 +1,3 @@
+from selenium.webdriver.common.by import By
+
+TITLE = (By.TAG_NAME, 'h3')
